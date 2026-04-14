9412edc8279a327126b35c198bf269de0317f056
Akilesh <ak1l38h@gmail.com>
2026-04-15 01:32:04 +0530

    Add Break Mode Fixed Clock Runtime Error

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let time = 25 * 60;
+const WORK_TIME = 25 * 60;
+const BREAK_TIME = 5 * 60;
+
+let mode = "work";
+let TOTAL_TIME = WORK_TIME;
+
+let startTime = null;
+let remainingTime = TOTAL_TIME;
 let timerRunning = false;
 let interval = null;
-let alarmSound = new Audio("FAHH.mp3");
+let alarmSound = new Audio("countdown_vecna.mp3");
 
-function updateDisplay() {
-  let minutes = Math.floor(time / 60);
-  let seconds = time % 60;
+function updateDisplay(timeLeft) {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
 
   document.getElementById("timer").innerText =
     `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
@@ -16,18 +23,25 @@ function startTimer() {
 
   timerRunning = true;
 
+  startTime = Date.now() - (TOTAL_TIME - remainingTime) * 1000;
+
   interval = setInterval(() => {
-    if (time <= 0) {
-  clearInterval(interval);
-  timerRunning = false;
 
-  alarmSound.play();
+    let elapsed = Math.floor((Date.now() - startTime) / 1000);
+    remainingTime = TOTAL_TIME - elapsed;
 
-  return;
-}
+    if (remainingTime <= 0) {
+      clearInterval(interval);
+      timerRunning = false;
 
-    time--;
-    updateDisplay();
+      updateDisplay(0);
+      alarmSound.play();
+
+      return;
+    }
+
+    updateDisplay(remainingTime);
+
   }, 1000);
 }
 
@@ -36,11 +50,44 @@ function stopTimer() {
   timerRunning = false;
 }
 
+function updateModeUI() {
+  let modeText = document.getElementById("mode");
+
+  if (mode === "work") {
+    modeText.innerText = "Work Time";
+  } else {
+    modeText.innerText = "Break Time";
+  }
+}
+
 function resetTimer() {
   clearInterval(interval);
   timerRunning = false;
-  time = 25 * 60;
-  updateDisplay();
+
+  mode = "work";
+  TOTAL_TIME = WORK_TIME;
+  remainingTime = TOTAL_TIME;
+
+  updateDisplay(remainingTime);
+  updateModeUI();
 }
 
-updateDisplay();
+function setMode(selectedMode) {
+  clearInterval(interval);
+  timerRunning = false;
+
+  mode = selectedMode;
+
+  if (mode === "work") {
+    TOTAL_TIME = WORK_TIME;
+  } else {
+    TOTAL_TIME = BREAK_TIME;
+  }
+
+  remainingTime = TOTAL_TIME;
+
+  updateDisplay(remainingTime);
+  updateModeUI();
+}
+
+updateDisplay(TOTAL_TIME);
